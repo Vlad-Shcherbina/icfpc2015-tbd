a690e9ce0e8cc25075421e9e03f2c7bff75fbb1e
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-08-06 07:45:03 +0200

    Update README; Add nix expression.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-See https://github.com/Vlad-Shcherbina/tbd-playground/blob/master/README.md
+ICFPC 2015
+===
+
+ + Installation and running guedlines are [in README of tbd-playground repository](https://github.com/Vlad-Shcherbina/tbd-playground/blob/master/README.md).
+ + [Guidelines for new members](https://docs.google.com/document/d/1gVkY6YPs4L8MBQY9usCRjXmp7wXSalfykFM4H2o4l2o/edit?usp=sharing).
+
+With [Nix package manager](http://nixos.org), getting a sandbox environment with everything installed is as easy as just running ``nix-shell`` or ``nix-shell --pure``.
---
+++ default.nix
@@ -0,0 +1,36 @@
+let
+  pkgs = import <nixpkgs> {};
+  hypothesisLatest = pkgs.python34Packages.buildPythonPackage rec {
+    name = "hypothesis-1.10.1";
+    doCheck = false;
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/source/h/hypothesis/hypothesis-1.10.1.tar.gz";
+      md5 = "e8086239a853e1e873cc8074dfccfa74";
+    };
+    meta = with pkgs.stdenv.lib; {
+      description = "A Python library for property based testing";
+      homepage = https://github.com/DRMacIver/hypothesis;
+      license = licenses.mpl20;
+    };
+  };
+
+in
+
+{ stdenv          ? pkgs.stdenv
+, python          ? pkgs.python3
+, nose            ? pkgs.python34Packages.nose
+, coverage        ? pkgs.python34Packages.coverage
+, hypothesis      ? hypothesisLatest
+, graphviz        ? pkgs.graphviz
+}:
+stdenv.mkDerivation {
+  name = "icfpc2015-tbd";
+  version = "0.1.0.0";
+  src = ./.;
+  buildInputs = [ python
+                  nose
+                  nosexcover
+                  coverage
+                  hypothesis
+                  graphviz ];
+}

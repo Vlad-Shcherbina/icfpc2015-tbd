604e93a99e7aa61f33c91870e986e4f21bdb0016
Anton Astafiev <astafiev.anton@gmail.com>
2015-08-08 23:11:24 +0200

    Added instructions for port tunneling.

--- production/golden/mount-ssh-fs.sh
+++
@@ -1,3 +0,0 @@
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-mkdir $DIR/sshfs-db
-sshfs dork@memorici.de:/home/dork/icfpc2015-tbd/production/golden/sshfs-db $DIR/sshfs-db -p 21984 -o IdentityFile=~/.ssh/id_rsa
---
+++ production/golden/port-tunnel.sh
@@ -0,0 +1,1 @@
+ssh dork@memorici.de -p 21984 -L 55135:localhost:55135 -i ~/.ssh/id_rsa

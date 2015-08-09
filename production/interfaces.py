16bdd6060d20e759c2add444bc70a2ad56b697d2
Anton Astafiev <astafiev.anton@gmail.com>
2015-08-09 17:36:41 +0200

    Another power phrase

--- production/interfaces.py
+++ production/interfaces.py
@@ -55,7 +55,8 @@ CHARS_BY_COMMAND = {cmd: chars for chars, cmd in COMMAND_CHARS if cmd}
 
 POWER_PHRASES = ["Ei!", "Ia! Ia!", "R'lyeh", "Yuggoth",
                  "ph'nglui mglw'nafh cthulhu r'lyeh wgah'nagl fhtagn.",
-                 "tsathoggua", "yogsothoth", "cthulhu fhtagn!"]
+                 "tsathoggua", "yogsothoth", "cthulhu fhtagn!",
+                 "john bigboote"]
 POWER_PHRASES = [w.lower() for w in POWER_PHRASES]
 
 

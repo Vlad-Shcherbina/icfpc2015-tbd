6c358ee4fd4eca6f530abe9313c412b970a0a2c2
Ioana Alexandra Antoche <ioana.antoche@yahoo.com>
2015-08-09 17:02:28 +0300

    script that compares the random strategy with the _choose_the_deapthest_randomly_. It plays 10 games for each map and shows the results.

--- ioana_scratch/phase_one.py
+++ ioana_scratch/phase_one.py
@@ -17,8 +17,7 @@ from production.golden import goldcfg
 from production.golden import api
 #from production.interfaces import GameEnded, Action
 #from production.cpp.placement import Graph
-total_score_random = 0;
-total_score_depthest = 0;
+
 
 
 def get_possible_placements(bsg):
@@ -45,6 +44,7 @@ def get_depthest_cell(placements):
         if y == max_y:
             result.append(position)
     return result
+
 def phase_one(initial_bsg):
     '''
     Return list of Placements nodes, where to lock units.
@@ -64,20 +64,58 @@ def phase_one(initial_bsg):
 
     return bsg, result
 
+def phase_one_random(initial_bsg):
+    '''
+    Return list of Placements nodes, where to lock units.
+    '''
 
-def main():
-    path = os.path.join(utils.get_data_dir(), 'qualifier/problem_0.json')
-    with open(path) as fin:
-        data = json.load(fin)
+    result = []
 
-    seeds = data['sourceSeeds']
-    bsg = big_step_game.BigStepGame.from_json(data, seeds[0])
-    print(bsg)
+    bsg = initial_bsg
+    while not bsg.game_ended:
+        possible_placements = get_possible_placements(bsg)
+        placement = random.choice(possible_placements)
+        result.append(placement)
+        bsg = bsg.lock_unit(placement)
+
+    return bsg, result
 
 
-    end_bsg, placments = phase_one(bsg)
+def main():    
+    for j in range(0, 25):
+        output_file = 'result_' + str(j)
+        with open(output_file, "w") as fout:
+            fout.write('')
+        input_file = 'qualifier/problem_' + str(j) + '.json'
+        path = os.path.join(utils.get_data_dir(), input_file)
+        with open(path) as fin:
+            data = json.load(fin)
 
-    print('Final score:', end_bsg.move_score)
+        seeds = data['sourceSeeds']
+        bsg = big_step_game.BigStepGame.from_json(data, seeds[0])
+        print(bsg)
+
+        total_score_random = 0.0
+        total_score_depthest = 0.0
+        total_games = 10
+
+        for i in range(total_games):
+            message = "Now playing game " + str(i) + "\n"
+            end_bsg, placments = phase_one(bsg)
+            total_score_depthest += end_bsg.move_score
+            message += 'Final score depthest:' + str(end_bsg.move_score) + '\n'
+            message += str(end_bsg) + '\n'
+            message += '============================= vs ==============================\n'
+            end_bsg, placments = phase_one_random(bsg)
+            total_score_random += end_bsg.move_score
+            message += 'Final score random:' + str(end_bsg.move_score) + '\n'
+            message += str(end_bsg) + '\n\n'
+            with open(output_file, "a") as fout:
+                fout.write(message)
+        depthest_avg = total_score_depthest / total_games;
+        random_avg = total_score_random / total_games;
+        with open(output_file, "a") as fout:
+                fout.write("depthest avg = " + str(depthest_avg) + "\nrandom avg = " + str(random_avg));
 
 
 if __name__ == '__main__':

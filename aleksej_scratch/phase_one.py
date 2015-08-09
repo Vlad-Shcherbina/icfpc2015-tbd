cfab80b7a8410de471ed2e5d5ae3c309a03f0e42
Aleksej Viktorcik <aviktorchik@google.com>
2015-08-09 19:43:10 +0200

    new neibour heiristics

--- aleksej_scratch/phase_one.py
+++ aleksej_scratch/phase_one.py
@@ -25,36 +25,62 @@ def get_possible_placements(bsg):
     return [bsg.get_placement_by_node_index(graph, node)
             for node in graph.GetLockedNodes()]
 
-def choice_placement(placements):
+
+def score_game_pos(game, last_move):
+    if game.ls_old:
+        return 10*9
+    heigh_score = min([y for x, y in last_move.get_members()])
+
+    density_score = 0
+    density_check = 0
+    for x, y in last_move.get_members():
+        xx = [x + i for i in range(-1, 1)]
+        yy = [y + i for i in range(-1, 1)]
+        has_neibour = [(a, b) in game.filled for a in xx for b in yy]
+        density_check += len(has_neibour)
+        density_score += sum(has_neibour)
+
+    return heigh_score + 3 * density_score / density_check
+
+
+def find_next_placement(game):
+    placements = get_possible_placements(game)
     assert(len(placements) > 0)
+    #return random.choice(placements)
+
     result = None
-    max_y = -1;
+    max_score = -10 ** 9;
     for p in placements:
-        y = min([y for x, y in p.get_members()])
-        if y > max_y:
-            max_y = y
+        moved_game = game.lock_unit(p)
+        score = score_game_pos(moved_game, p)
+        if score > max_score:
+            max_score = score
             result = p
+
+    assert(result)
     return result
 
 def play_game(game):
     result = []
+    turn_cnt = 0
     while not game.game_ended:
-        placements = get_possible_placements(game)
-
-        placement = choice_placement(placements)
-        #print(placement.get_members())
-
+        placement = find_next_placement(game)
         result.append(placement)
         game = game.lock_unit(placement)
+        turn_cnt += 1
+#        if turn_cnt % 10 == 0:
+#            print('current turn %d' % turn_cnt)
     return result, game
 
 
-def print_game_result(problem_id, seed, game, comment=''):
-    file_name = '%02d_%d.txt' % (problem_id, seed)
+def print_game_result(problem_id, games, comment=''):
+    file_name = '%02d.txt' % problem_id
     path = os.path.join('/tmp/', 'phase_one_solver', file_name)
     with open(path, 'a+') as f:
-        result = '%10d - %-100s\n' % (game.move_score, comment)
-        f.write(result)
+        for seed in sorted(games.keys()):
+            result = '%10d - %10d - %-100s\n' % (
+                seed, games[seed].move_score, comment)
+            f.write(result)
 
 
 def read_json(filename):
@@ -68,12 +94,16 @@ def main():
     problem_file_name = 'problem_%d.json' % problem_id
     path = os.path.join(utils.get_data_dir(), 'qualifier', problem_file_name)
     game_data = read_json(path)
-    seeds = game_data['sourceSeeds']
-    for seed in seeds:
+    all_games = dict()
+    for seed in game_data['sourceSeeds']:
         game = big_step_game.BigStepGame.from_json(game_data, seed)
         moves, game = play_game(game)
-        print_game_result(problem_id, seed, game, 'deepest min-max per figure')
+        all_games[seed] = game
         print(game)
+
+    #print_game_result(problem_id, all_games, 'deepest min-max per figure')
+    #print_game_result(problem_id, all_games, 'neighbor analysis')
+
 
 if __name__ == '__main__':
     main()

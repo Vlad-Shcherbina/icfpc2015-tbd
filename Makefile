6b74b2266851be3c806f71bd94f2c57f781645bb
Jonn Mostovoy <jm@memorici.de>
2015-08-10 08:14:38 +0300

    Psss sanity check

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 .PHONY: all install test
 
 all:
-	./build_helpers/nix_install.sh
 	nix-shell --run 'python3 -m production.test_all'
--- build_helpers/nix_install.sh
+++ build_helpers/nix_install.sh
@@ -1,1 +1,1 @@
-nix-env 2>&1 || curl https://nixos.org/nix/install | sh && . ~/.nix-profile/etc/profile.d/nix.sh
+nix-env 2>&1 || curl https://nixos.org/nix/install | sh

d148b6d72b3f2e44744afa9bf51918a13936fa48
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2015-08-10 10:24:37 +0200

    three power phrases

--- production/big_step_game.py
+++ production/big_step_game.py
@@ -35,7 +35,7 @@ class BigStepGame(object):
         bsg = BigStepGame()
 
         #bsg.dfa = cpp_placement.DFA()
-        bsg.dfa = translate_dfa(['ei!'])  # TODO
+        bsg.dfa = translate_dfa(interfaces.POWER_PHRASES[:3])  # TODO
 
         bsg.problem_id = json_data['problemId'] \
             if 'problemId' in json_data else -1

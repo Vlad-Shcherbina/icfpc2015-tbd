340928a16f89932813d345ce8fc3404a270a6424
Jonn Mostovoy <jm@memorici.de>
2015-08-10 07:47:13 +0300

    Don't require bash

--- build_helpers/nix_install.sh
+++ build_helpers/nix_install.sh
@@ -1,4 +1,1 @@
-if [ -z "$(nix-env 2>&1 | grep -o 'help')" ]; then
-	curl https://nixos.org/nix/install | sh
-	. ~/.nix-profile/etc/profile.d/nix.sh
-fi
+nix-env 2>&1 || curl https://nixos.org/nix/install | sh && . ~/.nix-profile/etc/profile.d/nix.sh
